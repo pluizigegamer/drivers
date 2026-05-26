6f9646fb02784c1916d526aa28815339129d7f58
pluizige_gamer <maxbarendregt1@gmail.com>
2026-05-26 17:22:06 +0200

    Add Electron main process file

---
+++ main.js
@@ -0,0 +1,16 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+function createWindow(){
+  const win = new BrowserWindow({
+    width:1200,
+    height:800,
+    backgroundColor:'#0C0C0C',
+    webPreferences:{
+      nodeIntegration:true,
+      contextIsolation:false
+    }
+  })
+  win.loadFile(path.join(__dirname,'index.html'))
+}
+app.whenReady().then(createWindow)
+app.on('window-all-closed', ()=> { if(process.platform!=='darwin') app.quit() })
